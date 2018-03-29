f5c3082b47a39422cf235305700b0f6e2a40ea9b
Graguan <03stevennguyen@gmail.com>
2018-03-29 14:32:17 -0400

    Update convToASCII.cpp

--- convToASCII.cpp
+++ convToASCII.cpp
@@ -1,13 +1,19 @@
 #include <string>
 #include <sstream>
+#include <vector>
 #include <iostream>
 using namespace std;
 
-void convToASCII(string letter)
+int convToASCII(string letter, int &array)
 {
 	for (int i = 0; i < letter.length(); i++)
 	{
-		char x = letter.at(i) - 48;
-		cout << int(x) << endl;
+		char x = letter.at(i);
+		for (int j = 0; i < 3; i++)
+		{
+			for (int k = 0; k < 3; k++)
+					array[j][k] = x;
+		}
 	}
+	return array;
 }
