86b297f1bdfcd4a91094481e13d164e67b325abc
parth0819 <37911862+parth0819@users.noreply.github.com>
2018-03-29 15:14:01 -0400

    Update getkey.h

--- getkey.h
+++ getkey.h
@@ -10,11 +10,9 @@ void getKey(void) {
 	srand(time(0));
 	int x;
 	while (1) {
-		for (int i = 0; i < 3; i++) {
-			for (int j=0; j<3; j++){
+		for (int i = 1; i <= 9; i++) {
 			x = rand() % 10000 + 1;
-			key[i][j] = x;
-			}
+			key[(i % 3) + 1][(i + 2) / 3] = x;
 		}
 		if ((key[1][1] * key[2][2] * key[3][3]) + key[1][2] * key[2][3] * key[3][1] + key[1][3] * key[2][1] * key[3][2] - key[3][1] * key[2][2] * key[1][3] - (key[3][2] * key[2][3] * key[1][1]) - (key[3][3] * key[2][1] * key[1][2]) != 0) {
 			break;
