2bb13f84164a39c493ef2763c721e10be38b1807
parth0819 <37911862+parth0819@users.noreply.github.com>
2018-03-29 15:02:58 -0400

    Update inverse.h

--- inverse.h
+++ inverse.h
@@ -7,16 +7,16 @@
 using namespace std;
 long double inv[3][3];
 void getinv(int arr[3][3]) {
-	double deter = (arr[1][1] * arr[2][2] * arr[3][3]) + arr[1][2] * arr[2][3] * arr[3][1] + arr[1][3] * arr[2][1] * arr[3][2] - arr[3][1] * arr[2][2] * arr[1][3] - (arr[3][2] * arr[2][3] * arr[1][1]) - (arr[3][3] * arr[2][1] * arr[1][2]);
+	double deter = (arr[0][0] * arr[1][1] * arr[2][2]) + arr[0][1] * arr[1][2] * arr[2][0] + arr[0][2] * arr[1][0] * arr[2][1] - arr[2][0] * arr[1][1] * arr[0][2] - (arr[2][1] * arr[1][2] * arr[0][0]) - (arr[2][2] * arr[1][0] * arr[0][1]);
 	//cout << deter<< endl;
-	inv[1][1] = (arr[2][2] * arr[3][3] - arr[2][3] * arr[3][2])/deter;
-	inv[2][1] = (arr[2][1] * arr[3][3] - arr[2][3] * arr[3][1])*-1/deter;
-	inv[3][1] = (arr[2][1] * arr[3][2] - arr[2][2] * arr[3][1])/deter;
-	inv[1][2] = (arr[1][2] * arr[3][3] - arr[1][3] * arr[3][2])*-1/deter;
-	inv[2][2] = (arr[1][1] * arr[3][3] - arr[1][3] * arr[3][1])/deter;
-	inv[3][2] = (arr[1][1] * arr[3][2] - arr[1][2] * arr[3][1])*-1/deter;
-	inv[1][3] = (arr[1][2] * arr[2][3] - arr[1][3] * arr[2][2])/deter;
-	inv[2][3] = (arr[1][1] * arr[2][3] - arr[1][3] * arr[2][1])*-1/deter;
-	inv[3][3] = (arr[1][1] * arr[2][2] - arr[1][2] * arr[2][1])/deter;
+	inv[0][0] = (arr[0][0] * arr[1][1] - arr[0][1] * arr[1][0])/deter;
+	inv[1][0] = (arr[1][0] * arr[2][2] - arr[1][2] * arr[2][0])*-1/deter;
+	inv[2][0] = (arr[1][0] * arr[2][1] - arr[1][1] * arr[2][0])/deter;
+	inv[0][1] = (arr[0][1] * arr[2][2] - arr[0][2] * arr[2][1])*-1/deter;
+	inv[1][1] = (arr[0][0] * arr[2][2] - arr[0][2] * arr[2][0])/deter;
+	inv[2][1] = (arr[0][0] * arr[2][1] - arr[0][1] * arr[2][0])*-1/deter;
+	inv[0][2] = (arr[0][1] * arr[1][2] - arr[0][2] * arr[1][1])/deter;
+	inv[1][2] = (arr[0][0] * arr[1][2] - arr[0][2] * arr[1][0])*-1/deter;
+	inv[2][2] = (arr[0][0] * arr[1][1] - arr[0][1] * arr[1][0])/deter;
 	//cout << inv[0][0] << endl;
 }
